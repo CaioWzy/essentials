c1f2bd742ebd6cea2aaa884b306060165d1214ff
Caio Rocha <caiowzy@gmail.com>
2025-02-19 02:33:25 -0300

    Add essentials.rb file

---
+++ essentials.rb
@@ -0,0 +1,17 @@
+class Essentials < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/CaioWzy/essentials/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "8dff1c3fb88440805841ad90748d899de7d96e7c77e2d150f23bab3c0a630c95"
+  license "Unlicense"
+
+  depends_on "wget"
+
+  def install
+    system "make", "install"
+  end
+
+  test do
+    system "echo", "Hello"
+  end
+end
